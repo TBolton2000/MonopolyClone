13a4f7865344aa498def05786db8ee7f24330d58
TBolton2000 <52944725+TBolton2000@users.noreply.github.com>
2019-12-04 22:27:55 -0600

    Added Classes and Token enum

---
+++ src/Player.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Player {
+	private int money;
+	private int ID;
+	private ArrayList<Property> property;
+	
+	private boolean isJailed;
+	private boolean isBankrupt;
+	
+}
---
+++ src/Token.java
@@ -0,0 +1,11 @@
+
+public enum Token {
+	Battleship("Battleship"), Boot("Boot"), Cat("Cat"), Racecar("Racecar"), ScottieDog("Scottie Dog"), 
+	Thimble("Thimble"), TopHat("Top Hat"), Wheelbarrow("Wheelbarrow");
+	
+	private String name;
+	Token(String name) {
+		this.name = name;
+	}
+	
+}
